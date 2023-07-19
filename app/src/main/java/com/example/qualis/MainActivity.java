5a54108dce01f628f3ce8c70637136f8d1210201
Henrique Soares <henrique.de.f.soares@gmail.com>
2023-07-19 17:05:48 -0400

    feat: replace old activity by new fragment

--- app/src/main/java/com/example/qualis/MainActivity.java
+++ app/src/main/java/com/example/qualis/MainActivity.java
@@ -5,8 +5,11 @@ import static com.example.qualis.R.id.button_correlacao_section;
 import static com.example.qualis.R.id.button_periodicos_section;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -21,6 +24,7 @@ import com.example.qualis.correlacao.CorrelacaoSection;
 
 import com.example.qualis.periodicos.PeriodicoSection;
 import com.example.qualis.periodicos.PeriodicoViewModel;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity{
     @Override
@@ -28,30 +32,35 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button buttonPeriodicos = findViewById(button_periodicos_section);
-        buttonPeriodicos.setOnClickListener(view -> startActivity(
-                new Intent(MainActivity.this, PeriodicoSection.class))
-        );
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        // Set the tabs to fill the entire layout.
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        // Use PagerAdapter to manage page views in fragments.
 
-        Button buttonConferencias = findViewById(button_conferencias_section);
-        buttonConferencias.setOnClickListener(view -> startActivity(
-                new Intent(MainActivity.this, ConferenciaSection.class))
-        );
+        // Use PagerAdapter to manage page views in fragments.
+        // Each page is represented by its own fragment.
+        final ViewPager viewPager = findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter
+                (getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        // Setting a listener for clicks.
 
-        Button buttonCorrelacao = findViewById(button_correlacao_section);
-        buttonCorrelacao.setOnClickListener(view -> startActivity(
-                new Intent(MainActivity.this, CorrelacaoSection.class))
-        );
+        // Setting a listener for clicks.
+        viewPager.addOnPageChangeListener(new
+                TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+           @Override
+           public void onTabSelected(TabLayout.Tab tab) {
+               viewPager.setCurrentItem(tab.getPosition());
+           }
 
-        Button buttonAtualizar = findViewById(R.id.button_update_database);
-        buttonAtualizar.setOnClickListener( view -> {
-                ConferenciaViewModel conferenciaViewModel = new ViewModelProvider(this).get(ConferenciaViewModel.class);
-                CorrelacaoViewModel correlacaoViewModel = new ViewModelProvider(this).get(CorrelacaoViewModel.class);
-                PeriodicoViewModel periodicoViewModel = new ViewModelProvider(this).get(PeriodicoViewModel.class);
-                periodicoViewModel.update();
-                correlacaoViewModel.update();
-                conferenciaViewModel.update();
-            }
-        );
+           @Override
+           public void onTabUnselected(TabLayout.Tab tab) {
+           }
+
+           @Override
+           public void onTabReselected(TabLayout.Tab tab) {
+           }
+       });
     }
 }
