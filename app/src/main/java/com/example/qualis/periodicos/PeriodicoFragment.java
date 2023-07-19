8bbc5a323b7fe8dec9931f3e52d0b1f03c682254
Henrique Soares <henrique.de.f.soares@gmail.com>
2023-07-19 18:20:30 -0400

    feat: activate periodico fragment

--- app/src/main/java/com/example/qualis/periodicos/PeriodicoFragment.java
+++ app/src/main/java/com/example/qualis/periodicos/PeriodicoFragment.java
@@ -2,65 +2,108 @@ package com.example.qualis.periodicos;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 import com.example.qualis.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PeriodicoFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.List;
+
 public class PeriodicoFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public PeriodicoFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PeriodicoFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PeriodicoFragment newInstance(String param1, String param2) {
-        PeriodicoFragment fragment = new PeriodicoFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_periodico, container, false);
+        View v = inflater.inflate(R.layout.fragment_periodico, container, false);
+
+        RecyclerView recyclerView = v.findViewById(R.id.sections_recycler_view);
+        final PeriodicoListAdapter adapter = new PeriodicoListAdapter(getActivity());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        PeriodicoViewModel periodicoViewModel = new ViewModelProvider(this).get(PeriodicoViewModel.class);
+        periodicoViewModel.getAllPeriodicos().observe(getActivity(), words -> {
+            // Update the cached copy of the words in the adapter.
+            adapter.setPeriodicos(words);
+        });
+
+        Spinner spinner = v.findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedCategory = (String) parent.getItemAtPosition(position);
+
+                if (selectedCategory.equals("Todos")) {
+                    List<Periodico> allPeriodicos = periodicoViewModel.getAllPeriodicos().getValue();
+
+                    adapter.setPeriodicos(allPeriodicos);
+                } else {
+                    List<Periodico> filteredPeriodicos = periodicoViewModel.filterPeriodicosByCategory(selectedCategory);
+
+                    adapter.setPeriodicos(filteredPeriodicos);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Do nothing
+            }
+        });
+
+
+
+        SearchView searchView = v.findViewById(R.id.search_view);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Handle the search query here
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // Filter the list of Periodico objects based on the search query
+                // Get the selected category and search query from the spinner and search view, respectively
+                String selectedCategory = (String) spinner.getSelectedItem();
+                String searchQuery = searchView.getQuery().toString();
+
+                // Filter the list of periodicos by the selected category and search query
+                List<Periodico> filteredPeriodicos = periodicoViewModel.searchAndFilterPeriodicos(searchQuery, selectedCategory);
+
+                // Update the list in the RecyclerView adapter with the filtered list
+                adapter.setPeriodicos(filteredPeriodicos);
+
+                // Check if the LiveData object is not null before accessing its data
+                // Use the post method of the RecyclerView object to update the adapter on the main thread
+                // after the background thread has finished filtering the list of periodicos
+                recyclerView.post(() -> adapter.setPeriodicos(filteredPeriodicos));
+
+
+                return false;
+            }
+        });
+
+        return v;
     }
 }
